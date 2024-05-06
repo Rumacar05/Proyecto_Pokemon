f0f922586bacb84ac45252f656ab9a3d2300379e
Rumacar05 <rumacar05@gmail.com>
2024-05-06 17:36:24 +0200

    Add: Main menu view and controller

--- src/main/java/controller/MenuController.java
+++ src/main/java/controller/MenuController.java
@@ -1,4 +1,51 @@
 package controller;
 
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import service.AlertService;
+
+import java.io.IOException;
+
 public class MenuController {
+
+    @FXML
+    private Button btnBattle;
+
+    @FXML
+    private Button btnManagePokemons;
+
+    @FXML
+    void btnBattle_clicked(MouseEvent event) {
+
+    }
+
+    @FXML
+    void btnManagePokemons_clicked(MouseEvent event) {
+
+    }
+
+    private void showStage(String viewFileName, String title) {
+        try {
+            FXMLLoader loader = FXMLLoader.load(getClass().getResource(viewFileName));
+
+            Parent root = loader.load();
+            Scene scene = new Scene(root);
+            Stage stage = new Stage();
+
+            stage.setTitle(title);
+            stage.initModality(Modality.APPLICATION_MODAL);
+            stage.setResizable(false);
+            stage.setScene(scene);
+            stage.showAndWait();
+        } catch (IOException ex) {
+            AlertService.showAlert(Alert.AlertType.ERROR, "Se ha producido un error cargando la ventana", ex.getMessage());
+        }
+    }
 }
--- src/main/java/ruben/proyecto_pokemon/PokemonApp.java
+++ src/main/java/ruben/proyecto_pokemon/PokemonApp.java
@@ -2,19 +2,30 @@ package ruben.proyecto_pokemon;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.stage.Stage;
+import service.AlertService;
 
 import java.io.IOException;
 
 public class PokemonApp extends Application {
     @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(PokemonApp.class.getResource("menu-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load());
-        stage.setTitle("Pokédex!");
-        stage.setScene(scene);
-        stage.show();
+    public void start(Stage stage) {
+        try {
+            FXMLLoader loader = new FXMLLoader(PokemonApp.class.getResource("menu-view.fxml"));
+
+            Parent root = loader.load();
+            Scene scene = new Scene(root);
+
+            stage.setTitle("Pokédex");
+            stage.setResizable(false);
+            stage.setScene(scene);
+            stage.show();
+        } catch (Exception ex) {
+            AlertService.showAlert(Alert.AlertType.ERROR, "Se ha producido una excepción no controlada", ex.getMessage());
+        }
     }
 
     public static void main(String[] args) {
