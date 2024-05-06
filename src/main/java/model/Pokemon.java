545b26d858f55e571ed894314b688040a67c7780
Rumacar05 <rumacar05@gmail.com>
2024-05-06 16:32:43 +0200

    Add: Pokemon model

---
+++ src/main/java/model/Pokemon.java
@@ -0,0 +1,94 @@
+package model;
+
+import enums.PokemonType;
+
+import java.util.Objects;
+
+public class Pokemon {
+    private int code;
+    private String name;
+    private PokemonType type;
+    private int level;
+    private int baseStrength;
+    private int baseDefense;
+    private int baseLife;
+
+    public Pokemon(int code, String name, PokemonType type, int level, int baseStrength, int baseDefense, int baseLife) {
+        this.code = code;
+        this.name = name;
+        this.type = type;
+        this.level = level;
+        this.baseStrength = baseStrength;
+        this.baseDefense = baseDefense;
+        this.baseLife = baseLife;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public PokemonType getType() {
+        return type;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    public int getBaseStrength() {
+        return baseStrength;
+    }
+
+    public int getBaseDefense() {
+        return baseDefense;
+    }
+
+    public int getBaseLife() {
+        return baseLife;
+    }
+
+    public int getStrength() {
+        return baseStrength * level;
+    }
+
+    public int getDefense() {
+        return baseDefense * level;
+    }
+
+    public int getLife() {
+        return baseLife * level;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Pokemon pokemon = (Pokemon) o;
+        return code == pokemon.code;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(code);
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("Pokemon{");
+
+        sb.append("code=").append(code);
+        sb.append(", name='").append(name).append('\'');
+        sb.append(", type=").append(type);
+        sb.append(", level=").append(level);
+        sb.append(", baseStrength=").append(baseStrength);
+        sb.append(", baseDefense=").append(baseDefense);
+        sb.append(", baseLife=").append(baseLife);
+        sb.append('}');
+
+        return sb.toString();
+    }
+}
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,4 +5,9 @@ module ruben.proyecto_pokemon {
 
     opens ruben.proyecto_pokemon to javafx.fxml;
     exports ruben.proyecto_pokemon;
+    opens controller to javafx.fxml;
+    exports controller;
+    opens model to javafx.fxml;
+    exports model;
+    exports enums;
 }
